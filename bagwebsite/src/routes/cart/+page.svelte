3ba511b9f5270a7d06d31dd788e6a45d6673d9b0
RivelleG <ST10083472@vcconnect.edu.za>
2023-10-24 18:46:54 +0200

    Shopping cart map fix

--- bagwebsite/src/routes/cart/+page.svelte
+++ bagwebsite/src/routes/cart/+page.svelte
@@ -1,6 +1,10 @@
 <!-- Script -->
 <script>
    	import { SHOPPING_CART } from '../../lib/global'; 
+       let shopping_cart;
+    SHOPPING_CART.subscribe((value) => {
+        shopping_cart = value;
+    });
 </script>
 
-{SHOPPING_CART.values}
+{SHOPPING_CART}
